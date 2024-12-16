c42701dd3475575287bdd571f9361651aa41d737
Deepak <deepakworkpc@gmail.com>
2024-12-16 23:03:23 +0530

    feat: inital commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// chrome.action.onClicked.addListener(() => {
+//     // Replace 'https://example.com' with your desired URL
+//     chrome.tabs.create({ url: "https://github.com/deep-vinci" });
+// });
+window.open("https://github.com/deep-vinci", '_blank').focus();
